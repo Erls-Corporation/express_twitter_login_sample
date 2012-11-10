5536665e2638d9694e9c351def707eae3ae8716c
tetu1984 <tetu1984@gmail.com>
2012-11-10 15:40:20 +0900

    Add login with twitter account ref: http://firn.jp/2011/06/08/node-express-twitter

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ var express = require('express')
 
 var app = express();
 
+var oauth = new (require('oauth').OAuth)(
+  'https://api.twitter.com/oauth/request_token',
+  'https://api.twitter.com/oauth/access_token',
+  'CONSUMER KEY',
+  'CONSUMER SECRET',
+  '1.0',
+  'http://127.0.0.1:3000/auth/twitter/callback',
+  'HMAC-SHA1'
+);
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -21,6 +31,10 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
+  app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+  });
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -31,6 +45,45 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+app.get('/auth/twitter', function(req, res) {
+  oauth.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results) {
+    if(error) {
+      res.send(error);
+    } else {
+      req.session.oauth = {};
+      req.session.oauth.token = oauth_token;
+      req.session.oauth.token_secret = oauth_token_secret;
+      res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + oauth_token);
+    }
+  });
+});
+
+app.get('/auth/twitter/callback', function(req, res) {
+  if(req.session.oauth) {
+    req.session.oauth.verifier = req.query.oauth_verifier;
+
+    oauth.getOAuthAccessToken(
+      req.session.oauth.token, req.session.oauth.token_secret, req.session.oauth.verifier, 
+      function(error, oauth_access_token, oauth_access_token_secret, results) {
+        if(error) {
+          res.send(error);
+        } else {
+          req.session.oauth.access_token = oauth_access_token;
+          req.session.oauth.access_token_secret = oauth_access_token_secret;
+          req.session.user_profile = results;
+          res.redirect('/');
+        }
+      }
+    );
+  }
+});
+
+app.get('/signout', function(req, res) {
+  delete req.session.oauth;
+  delete req.session.user_profile;
+  res.redirect('/');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
